fa06394b1fb9ee562abf8d5897ac6b0290c81cbe
gazifayaz <gazifayaz.16694@gmail.com>
2026-02-12 02:09:35 +0600

    fix: dockerfile hard-coded port removed Update: registraion form password field validation updated with minimum 1 chars

--- Task 4/Models/ViewModels/AuthViewModels.cs
+++ Task 4/Models/ViewModels/AuthViewModels.cs
@@ -14,7 +14,7 @@ public class RegisterViewModel
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required")]
-    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "Password must be at least 1 characters")]
     [DataType(DataType.Password)]
     public string Password { get; set; } = string.Empty;
 
